4577ff53d79ebaff6924abd232650b4f636a956a
Raphael M <rnikko3@gmail.com>
2021-08-02 23:23:11 +0900

    added contact links

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,18 @@
 <template>
-  <div class="max-w-screen-lg p-6 py-10 md:p-20 md:py-24 lg:p-10 lg:py-16" style="margin: 0 auto">
+  <div class="max-w-screen-lg p-6 py-10 md:p-20 md:py-24 lg:p-10 lg:py-16" style="margin: 0 auto; min-width: 375px">
     <LightSwitch />
     <Hello />
     <div class="space-y-8 md:space-y-10 lg:space-x-8 lg:space-y-0 lg:grid lg:grid-cols-2">
       <About />
       <Projects />
     </div>
+    <Contact class="my-8 md:my-10" />
   </div>
 </template>
 
 <script>
 import About from './components/About.vue'
+import Contact from './components/Contact.vue'
 import Hello from './components/Hello.vue'
 import LightSwitch from './components/LightSwitch.vue'
 import Projects from './components/Projects.vue'
@@ -19,6 +21,7 @@ export default {
   name: 'App',
   components: {
     About,
+    Contact,
     Hello,
     LightSwitch,
     Projects
---
+++ src/components/Contact.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="contact">
+    <!-- <p class="text-md md:text-xl font-semibold my-3 md:my-4" style="letter-spacing: -0.05 rem;">
+      Contact
+    </p> -->
+    <div class="space-x-6">
+      <div class="space-x-2 inline-flex">
+        <svg width="24" height="24" fill="currentColor">
+          <path fill-rule="evenodd" clip-rule="evenodd" d="M12 2C6.477 2 2 6.463 2 11.97c0 4.404 2.865 8.14 6.839 9.458.5.092.682-.216.682-.48 0-.236-.008-.864-.013-1.695-2.782.602-3.369-1.337-3.369-1.337-.454-1.151-1.11-1.458-1.11-1.458-.908-.618.069-.606.069-.606 1.003.07 1.531 1.027 1.531 1.027.892 1.524 2.341 1.084 2.91.828.092-.643.35-1.083.636-1.332-2.22-.251-4.555-1.107-4.555-4.927 0-1.088.39-1.979 1.029-2.675-.103-.252-.446-1.266.098-2.638 0 0 .84-.268 2.75 1.022A9.606 9.606 0 0112 6.82c.85.004 1.705.114 2.504.336 1.909-1.29 2.747-1.022 2.747-1.022.546 1.372.202 2.386.1 2.638.64.696 1.028 1.587 1.028 2.675 0 3.83-2.339 4.673-4.566 4.92.359.307.678.915.678 1.846 0 1.332-.012 2.407-.012 2.734 0 .267.18.577.688.48C19.137 20.107 22 16.373 22 11.969 22 6.463 17.522 2 12 2z">
+          </path>
+        </svg>
+        <span><a href="">Github</a></span>
+      </div>
+      <div class="space-x-2 inline-flex">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" style="padding: 2px" viewBox="0 0 20 20" fill="currentColor">
+          <path fill-rule="evenodd" d="M2.94 6.412A2 2 0 002 8.108V16a2 2 0 002 2h12a2 2 0 002-2V8.108a2 2 0 00-.94-1.696l-6-3.75a2 2 0 00-2.12 0l-6 3.75zm2.615 2.423a1 1 0 10-1.11 1.664l5 3.333a1 1 0 001.11 0l5-3.333a1 1 0 00-1.11-1.664L10 11.798 5.555 8.835z" clip-rule="evenodd" />
+        </svg>
+        <span><a href="">Email</a></span>
+      </div>
+      <div class="space-x-2 inline-flex">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" style="padding: 2px" viewBox="0 0 20 20" fill="currentColor">
+          <path fill-rule="evenodd" d="M4 4a2 2 0 012-2h4.586A2 2 0 0112 2.586L15.414 6A2 2 0 0116 7.414V16a2 2 0 01-2 2H6a2 2 0 01-2-2V4zm2 6a1 1 0 011-1h6a1 1 0 110 2H7a1 1 0 01-1-1zm1 3a1 1 0 100 2h6a1 1 0 100-2H7z" clip-rule="evenodd" />
+        </svg>
+        <span><a href="">Resume</a></span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Contact',
+}
+</script>
